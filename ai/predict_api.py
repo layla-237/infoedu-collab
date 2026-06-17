decca0e2558785dd02558c0694e318ef2abd1731
layla-237 <xlaylacoconutx@gmail.com>
2026-06-17 22:37:31 +0300

    Update predict_api.py

--- ai/predict_api.py
+++ ai/predict_api.py
@@ -11,28 +11,42 @@ model = joblib.load("model_admitere_random_forest.pkl")
 
 class AdmitereData(BaseModel):
     medie_elev: float
+    pozitie_elev: int
     sector: str
     profil: str
     specializare: str
     limba: str
     bilingv: str
     medie_liceu: float
+    ultima_pozitie_2024: int
+    ultima_pozitie_2023: int
+    ultima_pozitie_2022: int
+    pozitie_medie_intrare: int
+    diferenta_pozitie: int
 
 
 @app.post("/predict")
 def predict(data: AdmitereData):
     input_data = pd.DataFrame([{
         "medie_elev": data.medie_elev,
+        "pozitie_elev": data.pozitie_elev,
         "sector": data.sector,
         "profil": data.profil,
         "specializare": data.specializare,
         "limba": data.limba,
         "bilingv": data.bilingv,
-        "medie_liceu": data.medie_liceu
+        "medie_liceu": data.medie_liceu,
+        "ultima_pozitie_2024": data.ultima_pozitie_2024,
+        "ultima_pozitie_2023": data.ultima_pozitie_2023,
+        "ultima_pozitie_2022": data.ultima_pozitie_2022,
+        "pozitie_medie_intrare": data.pozitie_medie_intrare,
+        "diferenta_pozitie": data.diferenta_pozitie
     }])
 
-    probability = model.predict_proba(input_data)[0][1]
-    procent = round(probability * 100, 2)
+    predictie = model.predict(input_data)[0]
+    probabilitati = model.predict_proba(input_data)[0]
+
+    procent = round(probabilitati[1] * 100)
 
     if procent >= 75:
         nivel = "șanse mari"
@@ -44,6 +58,7 @@ def predict(data: AdmitereData):
         nivel = "șanse reduse"
 
     return {
+        "admis_model": int(predictie),
         "probabilitate": procent,
         "nivel": nivel
     }
